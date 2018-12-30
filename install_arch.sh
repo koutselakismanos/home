2d1ac92a6952a05a3562408a48fe4cf522736bc4
koutselakismanos <koutselakismanos@gmail.com>
2018-12-31 00:56:54 +0200

    changes to gitignore and added installation script

---
+++ install_arch.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+
+#refresh pacman
+# sudo pacman -Syy
+
+#packages
+sudo pacman -S git nitrogen termite rofi polybar python-pip ttf-font-awesome adobe-source-code-pro-fonts gcc make cmake code
