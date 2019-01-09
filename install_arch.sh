c1699f9671cded57ee50615567179a2c53c5cf4a
koutselakismanos <koutselakismanos@gmail.com>
2019-01-09 13:10:47 +0200

    more changes on packages

--- install_arch.sh
+++ install_arch.sh
@@ -5,4 +5,4 @@
 sudo pacman -Syy
 
 #packages
-sudo pacman -S git nitrogen termite rofi polybar python-pip ttf-font-awesome adobe-source-code-pro-fonts gcc make cmake code nvim chromium
+sudo pacman -S git nitrogen termite rofi polybar python-pip python2-pip ttf-font-awesome adobe-source-code-pro-fonts gcc make cmake code neovim chromium python2-neovim python-neovim
