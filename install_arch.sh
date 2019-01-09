c0e2661a7c2e9c4283de3c21dcf5a720b19412b7
koutselakismanos <koutselakismanos@gmail.com>
2019-01-09 13:01:10 +0200

    added some packages

--- install_arch.sh
+++ install_arch.sh
@@ -2,7 +2,7 @@
 
 
 #refresh pacman
-# sudo pacman -Syy
+sudo pacman -Syy
 
 #packages
-sudo pacman -S git nitrogen termite rofi polybar python-pip ttf-font-awesome adobe-source-code-pro-fonts gcc make cmake code
+sudo pacman -S git nitrogen termite rofi polybar python-pip ttf-font-awesome adobe-source-code-pro-fonts gcc make cmake code nvim chromium
