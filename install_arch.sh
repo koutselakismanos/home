66fde7cc54a110fbf6edd308b5d4d2db80e9dabe
koutselakismanos <koutselakismanos@gmail.com>
2019-01-12 14:44:23 +0200

    changed install_archh name to .install arch so its hidden, changed config of terminal

--- install_arch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-
-#refresh pacman
-sudo pacman -Syy
-
-#packages
-sudo pacman -S git nitrogen termite rofi polybar python-pip python2-pip ttf-font-awesome adobe-source-code-pro-fonts gcc make cmake code neovim chromium python2-neovim python-neovim
