acf12a61a8a82740735623b701f964bcad29d578
koutselakismanos <koutselakismanos@gmail.com>
2019-01-12 14:47:13 +0200

    changes to install_arch script

--- .install_arch.sh
+++ .install_arch.sh
@@ -5,6 +5,7 @@
 sudo pacman -Syy
 
 #packages
-sudo pacman -S git nitrogen termite yay rofi polybar python-pip python2-pip ttf-font-awesome adobe-source-code-pro-fonts gcc make cmake code neovim chromium python2-neovim python-neovim
+sudo pacman -S git nitrogen termite yay rofi polybar python-pip python2-pip ttf-font-awesome \
+adobe-source-code-pro-fonts gcc make cmake code neovim chromium python2-neovim python-neovim
 
 yay xcwd
