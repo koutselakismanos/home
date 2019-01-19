83b412859866c8c14d6c9d03e998b51a256f142c
koutselakismanos <koutselakismanos@gmail.com>
2019-01-19 22:24:43 +0200

    added ctags on install script

--- .install_arch.sh
+++ .install_arch.sh
@@ -7,6 +7,6 @@ sudo pacman -Syy
 #packages
 sudo pacman -S git nitrogen termite yay rofi polybar python-pip python2-pip ttf-font-awesome \
 adobe-source-code-pro-fonts gcc make cmake code neovim chromium python2-neovim python-neovim \
-yad xdotool
+yad xdotool ctags 
 
 yay xcwd
