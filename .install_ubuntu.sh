50498b13d183df0eea1309485a6b6fab946f857e
koutselakismanos <koutselakismanos@gmail.com>
2019-06-27 00:56:07 +0300

    changerou

--- .install_ubuntu.sh
+++ .install_ubuntu.sh
@@ -1,13 +1,21 @@
-echo "Hello fellow ubuntu user";
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root..."
+  exit
+fi
 
-apt update -y;
-apt upgrade -y;
+apt update -y
+apt upgrade -y
 
-apt install build-essential -y;
+# Essentials
+apt install build-essential vim-gtk3 neovim google-chrome-stable i3 pavucontrol rofi alsamixer compton -y
 
-# Required for polybar
-apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y;
-apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y;
+# Optional
+apt install python python3
 
-#
-yes | sudo apt install rofi -y;
+
+# Polybar installation
+apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y
+apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
+git clone https://github.com/polybar/polybar.git ~/Downloads/Polybar
+cd ~/Downloads/Polybar
+yes | ./build.sh
