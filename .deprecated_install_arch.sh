a9b54043829570cb28c446474c19a6856e2f10fd
koutselakismanos <koutselakismanos@gmail.com>
2019-08-17 18:16:14 +0300

    deprecated arch installation

---
+++ .deprecated_install_arch.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+
+#refresh pacman
+sudo pacman -Syyu
+
+#packages
+sudo pacman -S git nitrogen termite yay rofi polybar python-pip python2-pip ttf-font-awesome adobe-source-code-pro-fonts gcc make cmake code neovim chromium yad xdotool ctags 
+
+yay xcwd
