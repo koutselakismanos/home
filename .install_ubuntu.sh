d0ae19efe87854c89b82708719ae95934a4ddb33
koutselakismanos <koutselakismanos@gmail.com>
2019-08-29 18:48:13 +0300

    settings

--- .install_ubuntu.sh
+++ .install_ubuntu.sh
@@ -19,5 +19,5 @@ apt install python python3 -y
 apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y
 apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
 git clone https://github.com/polybar/polybar.git ~/Downloads/Polybar
-compton d ~/Downloads/Polybar
+# compton d ~/Downloads/Polybar
 yes | ./build.sh --all-features
